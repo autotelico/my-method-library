9d1dd70259cd28f8abcac0c8a093a01a9712ff2a
Henrique Heron <henriqueheron1@gmail.com>
2024-02-18 15:08:52 -0300

    improve readability

--- getLargestNumbers.js
+++ getLargestNumbers.js
@@ -1,19 +1,27 @@
-// This method allows you to get the largest numbers of any array.
-//      It takes in the amount of numbers you want to return
-//      and returns them in descending order.
+// This method allows you to get the largest numbers of a number array.
+// It takes in the amount of numbers you want to return
+// and returns the largest numbers in descending order.
 
-const numbersToReturn = [1, 4, 5645, 34, 3434, 65654]
+const exampleNumberList = [1, 4, 5645, 34, 3434, 65654];
 
 Array.prototype.getLargestnumbersToReturn = function (numbersToReturn) {
-    if (numbersToReturn > this.length) {
-        throw new Error('The provided argument must be lower than the array\'s length');
-    }
-    const sortedArray = this.sort((a, b) => a < b ? 1 : -1);
-    const result = [];
-    for (let i = 0; i < numbersToReturn; i++) {
-        result.push(sortedArray[i])
-    }
-    return result
-}
+  // Argument validation
+  if (numbersToReturn > this.length) {
+    throw new Error(
+      "The provided argument must be lower than the array's length"
+    );
+  }
+  if (typeof numbersToReturn !== 'number') {
+    throw new Error('The provided argument is not a number.');
+  }
+  // Calculation starts here
+  const sortedArray = this.sort((a, b) => (a < b ? 1 : -1));
+  const result = [];
+  for (let i = 0; i < numbersToReturn; i++) {
+    result.push(sortedArray[i]);
+  }
+  return result;
+};
 
-console.log(numbersToReturn.getLargestnumbersToReturn(5));
+// Try it! Experiment with different arguments.
+console.log(exampleNumberList.getLargestnumbersToReturn(3));
--- getLowestNumbers.js
+++ getLowestNumbers.js
@@ -1,15 +1,27 @@
-const numbers = [123, 4354353, 1, 0.2, 555, 1.3, 9400232, 42];
+// This method allows you to get the lowest values from a number array.
+// It takes in the amount of numbers you want to return from the array and
+// returns the lowest numbers in ascending order.
 
-Array.prototype.getLowestNumbers = function(numbersToReturn) {
-    if (numbersToReturn > this.length) {
-        throw new Error('The provided argument must be lower than the array\'s length');
-    }
-    const sortedArray = this.sort((a, b) => a < b ? -1 : 1);
-    const result = [];
-    for (let i = 0; i < numbersToReturn; i++) {
-        result.push(sortedArray[i])
-    }
-    return result;
-}
+const exampleNumberList = [123, 4354353, 1, 0.2, 555, 1.3, 9400232, 42];
 
-console.log(numbers.getLowestNumbers(2))
+Array.prototype.getLowestNumbers = function (numbersToReturn) {
+  // Argument validation
+  if (numbersToReturn > this.length) {
+    throw new Error(
+      "The provided argument must be lower than the array's length"
+    );
+  }
+  if (typeof numbersToReturn !== 'number') {
+    throw new Error('The provided argument is not a number.');
+  }
+  // Calculation starts here
+  const sortedArray = this.sort((a, b) => (a < b ? -1 : 1));
+  const result = [];
+  for (let i = 0; i < numbersToReturn; i++) {
+    result.push(sortedArray[i]);
+  }
+  return result;
+};
+
+// Try it! Experiment with different arguments.
+console.log(exampleNumberList.getLowestNumbers(2));
